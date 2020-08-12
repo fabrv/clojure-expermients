47b60ac022acf135864727337d28f45adb58b6db
Fabrizio Delcompare <frivera@nevicad.com>
2020-08-12 19:09:30 +0000

    full min to dec

--- main.clj
+++ main.clj
@@ -1,11 +1,13 @@
 ;; Bin To Dec
-(def x "1101")
+(def x "11001101")
 
 (defn getKey [el]
-  (println (get el (nth (keys el) 0)))
-  (Math/pow 2 (nth (keys el) 0))
+  (*
+    (Character/digit (get el (nth (keys el) 0)) 10)
+    (Math/pow 2 (nth (keys el) 0))
+  )
 )
 
 (def binKeyed (map-indexed hash-map  (reverse x)))
 
-(map getKey binKeyed)
+(int (reduce + (map getKey binKeyed)))
